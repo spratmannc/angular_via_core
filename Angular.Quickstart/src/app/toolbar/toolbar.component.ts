05352799705108ace322b02cd78560d1733b163c
Roger Spratley <roger_spratley@hotmail.com>
2017-05-19 01:17:29 -0400

    Added some coolness

--- Angular.Quickstart/src/app/toolbar/toolbar.component.ts
+++ Angular.Quickstart/src/app/toolbar/toolbar.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class ToolbarComponent {
 
+    someText: string = "Click button below to see coolness";
+    private count: number = 0;
+
+    startTimer() {
+        console.log("Starting timer");
+
+        setInterval(() => {
+            this.count++;
+            this.someText = `The count is now ${this.count}`;
+        }, 1000);)
+    }
 }
